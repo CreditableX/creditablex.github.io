33f6293f463cfd4e64d5ac6a7df5db53faccfd21
CreditableX <creditablecreates@gmail.com>
2023-11-12 15:48:45 +0800

    attempt 3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
 let n = 0;
 
-function addone(n) {
+setinterval(function addone(n) {
     n += 1;
     document.getElementById("times").innerHTML = n;
-}
+}, 500)
