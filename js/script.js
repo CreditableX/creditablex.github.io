921ad02cf61cfccd5771abd4566e207b4802811f
CreditableX <creditablecreates@gmail.com>
2023-11-12 16:14:26 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-
-var timesClicked = 0;
-
-function btnClick(){
-    timesClicked ++;
-
-    document.getElementById('timesClicked').innerHTML = timesClicked;
-    return true
-}
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
