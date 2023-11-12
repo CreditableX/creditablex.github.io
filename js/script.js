781ca5b171e44431d3dc8f652afd204da87171dc
CreditableX <creditablecreates@gmail.com>
2023-11-12 16:00:49 +0800

    helpppp

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-let n = 0;
+var timesClicked = 0;
 
-incrementVal = function(eve){
-    var _ele = $(eve.target);
-    var _val = parseInt(_ele.text(),10);
-    _ele.text(_val+1);
+ function btnClick(){
+    timesClicked ++;
+     
+    document.getElementById('timesClicked').innerHTML = timesClicked;
+    return true 
 }
