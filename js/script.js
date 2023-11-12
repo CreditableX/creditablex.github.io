67a905c6f785e53c3a8a61bdddd06a85cb2d514d
CreditableX <creditablecreates@gmail.com>
2023-11-12 15:31:42 +0800

    button testing + onclick

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+let n = 0;
+
+function addone(n) {
+    n += 1;
+    document.body.innerHTML = "<h1>Times clicked: " + n + "</h1>";
+}
